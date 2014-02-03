80a153e8560f1905e4491985779a7cb351c6cece
jnolascob <jnolascob@gmail.com>
2014-02-03 17:33:42 -0500

    index -v2

--- ws/index.php
+++ ws/index.php
@@ -1,7 +1,7 @@
 <?php
 require_once '../include/DbHandler.php';
 require_once '../include/PassHash.php';
-require '../libs/Slim/Slim.php';
+require_once '../libs/Slim/Slim.php';
  
 \Slim\Slim::registerAutoloader();
  
