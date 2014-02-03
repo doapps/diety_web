f0d4b9c2e171b1e5c270a854eb7871a51f945bdd
jnolascob <jnolascob@gmail.com>
2014-02-03 17:27:01 -0500

    index

--- ws/index.php
+++ ws/index.php
@@ -1,7 +1,7 @@
 <?php
 require_once '../include/DbHandler.php';
 require_once '../include/PassHash.php';
-require '.././libs/Slim/Slim.php';
+require '../libs/Slim/Slim.php';
  
 \Slim\Slim::registerAutoloader();
  
